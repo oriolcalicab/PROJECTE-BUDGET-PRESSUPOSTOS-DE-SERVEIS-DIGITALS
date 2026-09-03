aa569acea0deba4e79dd40efb18fbc5ff3d71097
Oriol <oriolcalicab@gmail.com>
2026-09-03 15:40:51 +0200

    feat: remove navigation bar and simplify routes to single page

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,20 +1,14 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { CreateQuote } from "../pages/CreateQuote";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "../pages/home";
 import { QuoteDetail } from "../pages/QuoteDetail";
-import { History } from "../pages/History";
 
 
 export function AppRouter() {
   return (
     <BrowserRouter>
-      <nav className="p-4 border-b flex gap-4">
-        <Link to="/">New quote</Link>
-        <Link to="/history">Quote History</Link>
-      </nav>
       <Routes>
-        <Route path="/" element={<CreateQuote />} />
-        <Route path="/quote/:id" element={<QuoteDetail />} />
-        <Route path="/history" element={<History />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/quote/:id" element={<QuoteDetail />} />        
       </Routes>
     </BrowserRouter>
   );
